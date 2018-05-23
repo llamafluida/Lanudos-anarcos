dd104bee6e2c562e9d7088f64c4631df6f3cb047
Iván Gutiérrez <37635593+llamafluida@users.noreply.github.com>
2018-05-22 20:12:53 -0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,82 +1,99 @@
-$(document).ready(function(){
-    //Aquí empieza la galería
-//primer un objeto donde están almacenados los src y el texto de las imágenes
-    let galeria = [{  "src": "./images/galeria1.jpg",
-    "texto": "Miembros de la banda en foto grupal"
-    },
-    {
-        "src": "./images/galeria2.jpg",
-        "texto": "Fufi disfrutando de su bebida"
-    },
-    {
-        "src": "./images/galeria3.jpg",
-        "texto": "Puitas disfrutando de su bebida"
-    },
-    {
-        "src": "./images/galeria4.jpg",
-        "texto": "A Fufi se le pasaron un poco las copas"
-    },
-    {
-        "src": "./images/galeria5.jpg",
-        "texto": "El resultado de una noche de juerga"
+let galeria = [{  "src": "./images/galeria1.jpg",
+"texto": "Miembros de la banda en foto grupal"
+},
+{
+    "src": "./images/galeria2.jpg",
+    "texto": "Fufi disfrutando de su bebida"
+},
+{
+    "src": "./images/galeria3.jpg",
+    "texto": "Puitas disfrutando de su bebida"
+},
+{
+    "src": "./images/galeria4.jpg",
+    "texto": "A Fufi se le pasaron un poco las copas"
+},
+{
+    "src": "./images/galeria5.jpg",
+    "texto": "El resultado de una noche de juerga"
+}
+]
+//galeria página principal
+let i = 0;
+//contenedor de imágenes y texto debajo
+const slide = document.getElementById("slide");
+const slideText = document.getElementById("slide-text");
+//flechas
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+//colocando la primera imagen y texto dentro de la página
+if(slide != null && slideText != null){
+slide.src = galeria[i].src;
+slideText.innerText = galeria[i].texto;
+
+//función de la flecha hacia adelante
+next.addEventListener("click", function(){
+    if(i < galeria.length - 1) {
+        i++;
+        slide.src = galeria[i].src
+        slideText.innerText = galeria[i].texto
     }
-    ]
-//variable que sirve para indicar el index de la imagen actual
-    let i = 0;
-// variables que contienen los id de los elementos donde estará la imagen y el texto
-    const slide = $("#slide");
-    const slideText = $("#slide-text");
-//con esto cuando se carga la página se coloca la primera imagen y texto de la var galeria
-    slide.attr("src", galeria[i].src);
-    slideText.text(galeria[i].texto);
-//Aquí esto se activa al apretar la flecha izquierda, que tiene la id next
-    $("#next").click(function(){
-        if(i < galeria.length - 1) {
-            i++;
-           slide.attr("src", galeria[i].src);
-           slideText.text(galeria[i].texto);
-        }
-        else {
-            i = 0;
-            slide.attr("src", galeria[i].src);
-            slideText.text(galeria[i].texto);
-        }
-
-
-    });
-    $("#prev").click(function(){
-        if(i != 0) {
-            i--;
-            slide.attr("src", galeria[i].src)
-            slideText.text(galeria[i].texto);
-        }
-        else {
-            i = galeria.length - 1;
-            slide.attr("src", galeria[i].src)
-            slideText.text(galeria[i].texto);
-        }
-    });
-    //Aquí termina la galería
-    //Aquí comienza el botón del header
-    const headerLinks = $("header nav a:nth-child(n+2)");
-    $("#toggle-div-button button").click(function(){
-        headerLinks.toggleClass("hide-link");
-    });
-
-    //galeria
-    const imgGaleria = $("#galeria-img-principal");
-    const txtGaleria = $("#galeria-texto-principal");
-    imgGaleria.attr("src", galeria[0].src);
-    txtGaleria.text(galeria[0].texto);
-
-    for(let iGaleria = 0; iGaleria < galeria.length || iGaleria < imgGaleria.length; iGaleria++) {
-        document.getElementsByClassName("galeria-img")[iGaleria].src = galeria[iGaleria].src;
-        document.getElementsByClassName("galeria-img")[iGaleria].alt = galeria[iGaleria].texto;
+    else {
+        i = 0;
+        slide.src = galeria[i].src;
+        slideText.innerText = galeria[i].texto;
     }
-
-    $(".galeria-img").click(function(){
-        $(imgGaleria).attr("src", this.src);
-        $(txtGaleria).text(this.alt);
-    });
 });
+//función de la flecha hacia atrás
+prev.addEventListener("click", function() {
+    if(i != 0) {
+        i--;
+        slide.src = galeria[i].src
+        slideText.innerText = galeria[i].texto
+    }
+    else {
+        i = galeria.length - 1;
+        slide.src = galeria[i].src;
+        slideText.innerText = galeria[i].texto;
+    }
+});
+}
+//botón header 
+const toggleButtonHeader = document.getElementById("toggle-button-header");
+const hideLink = document.getElementsByClassName("hideLink");
+toggleButtonHeader.addEventListener("click", function(){
+for(let j = 0; j < hideLink.length; j++){
+    hideLink[j].classList.toggle("hide-link");
+}
+});
+//página galería
 
+const galeriaImg = document.getElementsByClassName("galeria-img");
+if(galeriaImg[0] != null){
+for(let iGaleria = 0; iGaleria < galeria.length; iGaleria++){
+//Tenía un problema poniendo un || luego de la condición, por lo que lo dividí, la segunda parte es el if abajo
+    if(iGaleria < galeriaImg.length){
+    galeriaImg[iGaleria].setAttribute("src", galeria[iGaleria].src);
+    galeriaImg[iGaleria].setAttribute("alt", galeria[iGaleria].texto);
+    }
+}
+const toggleVisor = document.getElementById("toggle-visor");
+const galeriaImgPrincipal = document.getElementById("galeria-img-principal");
+const galeriaTextoPrincipal = document.getElementById("galeria-texto-principal");
+const closeVisor = document.getElementById("close-visor");
+for(let k = 0; k < galeriaImg.length; k++){
+galeriaImg[k].addEventListener("click", function() {
+    toggleVisor.style.display = "flex";
+    galeriaImgPrincipal.setAttribute("src", this.src);
+    galeriaTextoPrincipal.innerText = this.alt;
+});
+}
+closeVisor.addEventListener("click", function() {
+    toggleVisor.style.display = "none";
+});
+window.addEventListener("click", function(e) {
+    if(e.target == toggleVisor) {
+        toggleVisor.style.display = "none";
+    }
+});
+}
